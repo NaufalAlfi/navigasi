38669142fecbb3f691c715e7f83c9518cdb01641
NaufaAlfi <naufalalfi100@gmail.com>
2022-05-29 14:16:31 +0700

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+import 'package:navigasi/navigations/screen.dart';
+
+class Todo {
+  final String title;
+  final String description;
+
+  const Todo(this.title, this.description);
+}
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Passing Data',
+      home: TodosScreen(
+        todos: List.generate(
+          20,
+          (i) => Todo(
+            'Todo $i',
+            'A description of what needs to be done for Todo $i',
+          ),
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/navigations/screen.dart
@@ -0,0 +1,6 @@
+export 'navPart1.dart';
+export 'navPart2.dart';
+export 'navPart3.dart';
+export 'navPart4.dart';
+export 'navPart5.dart';
+export 'navPart6.dart';
